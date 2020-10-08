84e259c7c202642b583822439ba4b864ccb18a94
loonyT <62712207+loonyT@users.noreply.github.com>
2020-10-08 22:42:10 +0200

    Update authenticate.js

--- api/middleware/authenticate.js
+++ api/middleware/authenticate.js
@@ -2,12 +2,14 @@ const jwt = require('jsonwebtoken');
 
 const authenticate = (req, res, next) => {
 
-    try{
+    try{ // puisque de nombreuses erreurs peuvent apparaitre, le bloc try catch est nécessaire pour cette étape. 
 
          //Extract Authorization Token
+      
         const token = req.headers["auth-token"];
+          // après l'extraction du Token, il faut le décoder 
         const decoded = jwt.verify(token, 'mysecretkey');
-        next();
+        next(); // passage à l'exécution grâce à la fonction next 
 
     }catch(error){
         res.status(500).json({
